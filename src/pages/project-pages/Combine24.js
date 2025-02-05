03e980ee7c5b21e88ce7ba9b1946021bc2bbeade
Alvar Mahlberg <alvar@AvlarMahlbergMBP.local>
2025-02-06 00:17:12 +0200

    Päivitetty projekti Verceliin

--- src/pages/project-pages/Combine24.js
+++ src/pages/project-pages/Combine24.js
@@ -2,13 +2,15 @@ import React from "react";
 
 // Kuvien importit
 import combine1 from "../../assets/images/combine/image1.jpg";
+import combine1Mobile from "../../assets/images/combine/image1-mobile.jpg"; // Mobiiliversio
 
 function Combine24() {
   return (
     <div className="fullscreen-project">
       <div className="project-background">
         <img 
-          src={combine1}
+          src={combine1} 
+          srcSet={`${combine1Mobile} 600w, ${combine1} 1200w`} // Mobiiliversio alle 600px
           alt="Combine24 visualization"
           className="background-image"
         />
@@ -16,7 +18,7 @@ function Combine24() {
 
       <div className="project-info">
         <h1>Combine24</h1>
-        <p className="project-details">A new museum-led digital art competition</p>
+        <p className="project-details">A global digital art competition in 2024</p>
       </div>
     </div>
   );
